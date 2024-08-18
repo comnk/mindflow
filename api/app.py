af14ad0eefb00cc1ac88621fa0677ef35758d40c
comnk <vhsiao36@gmail.com>
2024-08-18 00:11:14 -0700

    second potential fix

--- api/app.py
+++ api/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import cross_origin
 
 import chat
 
@@ -7,5 +8,6 @@ app = Flask(__name__)
 app.add_url_rule("/api/journal-chatbot/", view_func=chat.chatbot, methods=['get', 'post'])
 
 @app.route("/api/homepage")
+@cross_origin()
 def homepage():
     return {"message": "Hello World!"}
