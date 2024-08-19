096fb035952bac9acf1a16740adff9e28fb8232f
comnk <vhsiao36@gmail.com>
2024-08-18 17:07:17 -0700

    more changes

--- api/app.py
+++ api/app.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_cors import cross_origin, CORS
 
 import chat
+import mindfulness
 
 app = Flask(__name__)
 CORS(app)
 
 app.add_url_rule("/api/journal-chatbot/", view_func=chat.chatbot, methods=['get', 'post'])
+app.add_url_rule("/api/mindfulness-video/",view_func=mindfulness.get_meditation_video, methods=['get', 'post'] )
 
 @app.route("/api/homepage", methods=["GET"])
 @cross_origin()
