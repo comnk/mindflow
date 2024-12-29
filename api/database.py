0da11391eba7ea5209f71c5bb50585d3e116f100
comnk <vhsiao36@gmail.com>
2024-12-28 18:51:42 -0800

    fix

--- api/database.py
+++ api/database.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 MONGODB_URI = os.environ['MONGODB_URI']
 
-client = MongoClient(os.getenv("MONGODB_URI"), serverSelectionTimeoutMS=50000)
+client = MongoClient(os.getenv("MONGODB_URI"))
 
 def get_db():
     return client["mindflow"]
