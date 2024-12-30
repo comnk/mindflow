fc8daa8d539bde2df83d89030e6f10f188cc1005
comnk <vhsiao36@gmail.com>
2024-12-29 23:30:18 -0800

    database test fix

--- api/database.py
+++ api/database.py
@@ -5,7 +5,12 @@ import os
 load_dotenv()
 MONGODB_URI = os.environ['MONGODB_URI']
 
-client = MongoClient(os.getenv("MONGODB_URI"))
+_client = None  # Use a global variable to cache the client
 
 def get_db():
-    return client["mindflow"]
+    global _client
+    if _client is None:
+        # Initialize MongoClient only when called
+        _client = MongoClient(os.getenv("MONGODB_URI"))
+    
+    return _client["mindflow"]
